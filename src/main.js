b980bf668b2fcd8a2cf1e1ef5449e7768b588d68
Ivan Vilanculo <vilanculoivan@gmail.com>
2019-02-24 13:16:21 +0200

    add firebase

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import firebase from 'firebase'
 import App from './App'
 import router from './router'
 import store from './store'
@@ -8,6 +9,17 @@ import AppDate from './components/AppDate'
 Vue.component('app-date', AppDate)
 
 Vue.config.productionTip = false
+
+// Initialize Firebase
+const config = {
+  apiKey: 'AIzaSyB89QwflwRgyXkllFdR7ZcTofuCt12iWg0',
+  authDomain: 'vue-school-forum-9dd3f.firebaseapp.com',
+  databaseURL: 'https://vue-school-forum-9dd3f.firebaseio.com',
+  projectId: 'vue-school-forum-9dd3f',
+  storageBucket: 'vue-school-forum-9dd3f.appspot.com',
+  messagingSenderId: '381885373373'
+}
+firebase.initializeApp(config)
 
 /* eslint-disable no-new */
 new Vue({
